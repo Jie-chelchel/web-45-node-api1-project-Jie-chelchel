ea98d2e54931efcf8cda3135710225e6a1acd0b4
Jie Zhang <zhangjiew@gmail.com>
2021-08-30 13:06:47 -0600

    test push

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server');
-
+const server = require("./api/server");
+const express = require("express");
+server.use(express.json());
 const port = 5000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, () => {
+  console.log(`Listening on port :${port} `);
+});
