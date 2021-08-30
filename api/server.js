a8f01e84aedc468c8c6b078202b7cbad5b17cd04
Jie Zhang <zhangjiew@gmail.com>
2021-08-30 13:33:38 -0600

    get users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,26 @@
 // BUILD YOUR SERVER HERE
+const {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+  resetDB,
+} = require("./users/model.js");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express");
+const server = express();
+
+server.get("/api/users", (req, res) => {
+  find()
+    .then((users) => res.status(200).json(users))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+server.use("*", (req, res) => {
+  res.send("test test");
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server");
-const express = require("express");
-server.use(express.json());
+
+// server.use(express.json());
 const port = 5000;
 
 // START YOUR SERVER HERE
